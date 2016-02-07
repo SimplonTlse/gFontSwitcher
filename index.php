41db295bc0e2f78eea7fa8673f51ecbacadf1cfd
Amine EL G <aelghayate@simplon.co>
2016-02-07 09:39:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+// ce fichier est le point d'entrée de notre application
 
+//on charge le fichier qui contient nos fonctions utilitaires
 require_once 'lib/functions.php';
 
 //routing
-
+// on charge notre vue
 require 'views/show.part.php';
