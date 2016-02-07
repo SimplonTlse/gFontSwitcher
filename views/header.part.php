15222dfefdc0c5da31ee372f6667eaa1999fde03
Amine EL G <aelghayate@simplon.co>
2016-02-07 09:45:02 +0100

    Update header.part.php

--- views/header.part.php
+++ views/header.part.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Test de la font : <?php echo getFont(); ?></title>
 	<link rel="stylesheet" href="assets/css/style.css">
 <link 
 href='https://fonts.googleapis.com/css?family=<?php echo getFont(); ?>' 
