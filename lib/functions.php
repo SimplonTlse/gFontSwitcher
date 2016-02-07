c8b73f3f041e263e989f30e721a37c55fc80cd51
Amine EL G <aelghayate@simplon.co>
2016-02-07 09:48:06 +0100

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -5,13 +5,17 @@ recupere la clé font du tableau $_GET
 ou renvoie une valeur par défaut
 */
 function getFont() {
+	//on test si la clé 'font' est définie dans le tableau $_GET
 	if(isset($_GET['font'])) {
+		// si oui on retourne immédiatemment la valeur associée, ce qui interrompt la fonction 
 		return $_GET['font'];
 	} 
+	// et sinon on retourne une valeur par défaut
 	return 'Inconsolata';
 }
 
 function makeMenu(){
+	//le tableau de notre séléction de fonts
 	$fonts = [
 		'Inconsolata', 
 		'Indie Flower', 
@@ -19,7 +23,7 @@ function makeMenu(){
 		'Great Vibes',
 		'Cookie'
 	];
-
+	//on boucle sur le tableau précédent et on affiche un lien avec les bons paramètres
 	foreach($fonts as $font){
 		echo '<a href="?font=' . $font . '">' . $font . '</a>';
 	}
